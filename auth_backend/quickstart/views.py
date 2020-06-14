1576e4f8a216dff32af4019738b8654a8f75a943
CleverShovel <vladimirpanov73@gmail.com>
2020-06-14 13:31:18 +0400

    init

---
+++ auth_backend/quickstart/views.py
@@ -0,0 +1,13 @@
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+# Create your views here.
+
+@api_view(['POST'])
+def authenticate(request):
+    return Response(None, status=status.HTTP_200_OK)
+
+@api_view(['POST'])
+def registrate(request):
+    return Response(None, status=status.HTTP_200_OK)
